8ee74d6e66bc23e079b63f3c5d9b7c9bb11e0420
Unknown <Daniel.j.hart1995@gmail.com>
2018-11-23 23:31:08 +0000

    Inter file linking added Now get a reference for all files so that a directory can be made on the side.

--- Harwiki/Harwiki.py
+++ Harwiki/Harwiki.py
@@ -8,12 +8,15 @@ header = "<!DOCTYPE html>\n<html>\n"
 footer = "\n</html>"
 wikiName = ""
 handlingTable = False
+listOfPages = list()
+
 # 
 
 # Functions
 
 def IsTableItem(x: str):
 	return x == Constant.TABLEIMAGE or x == Constant.TABLEROW
+# ~IsTableItem
 
 def SetTagsAndReplace(begTag: str, endTag: str, line: str, replace: str, repWith: str):
 	line = line.replace(replace + " ", repWith, 1)  
@@ -24,6 +27,7 @@ def SetTagsAndReplace(begTag: str, endTag: str, line: str, replace: str, repWith
 		handlingTable = False
 	
 	return begTag, endTag, line
+# ~SetTagsAndReplace
 
 def FormatLineToHTML(line: str):
 	begTag = ""
@@ -63,17 +67,39 @@ def FormatLineToHTML(line: str):
 			closingBrace = line.find(Constant.LINKEND, i)
 			openingBracket = line.find(Constant.LINKTBEG, closingBrace)
 			closingBracket = line.find(Constant.LINKTEND, openingBracket)
-			content += "<a href =\"" + line[i + 1 : closingBrace] + "\">" + line[openingBracket + 1 :closingBracket] + "</a>"
+			linkText = line[i + 1 : closingBrace]
+			htmlLinkText = ""
+			
+			if linkText.startswith("https"):
+				htmlLinkText = linkText
+			else:
+				fullPath = ""
+				for page in listOfPages:
+					if page.find(linkText) > - 1:
+						fullPath = page[:-5]
+				
+				if fullPath == "":
+					print("Could not find link: " + linkText)
+					continue
+				
+				correctedText = fullPath
+				correctedText = correctedText.replace(wikiName + "/", "")
+				correctedText = correctedText.replace(" ", "%20")
+				correctedText = correctedText + ".html"
+				htmlLinkText = correctedText
+
+			content += "<a href =\"" + htmlLinkText + "\">" + line[openingBracket + 1 :closingBracket] + "</a>"
 			skipTo = closingBracket + 1
 		else:
 			content += c
 
 	return begTag + content + endTag
-	
+# ~FormatLineToHTML
 
 def CreatePage(name: str):
-	file = open(wikiName + "/" + name, "r")
-	name = name.replace(".txt", "")
+	global wikiName
+	file = open(name, "r")
+	name = name.replace(".wiki", "")
 	print("Creating page: " + name)
 
 	fileContent = file.readlines()
@@ -81,28 +107,44 @@ def CreatePage(name: str):
 	construct = header
 	construct += "<head>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"shared.css\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\n<title>" + name + "</title>\n</head>"
 	construct += "\n<body>\n<div id=\"content\" class=\"mw-body\" role=\"main\">\n<div id=\"bodyContent\" class=\"mw-body-content\">"
-   
+
 	for c in fileContent:
 		construct += FormatLineToHTML(c)
 	
-	construct += "\n</div>\n</div>\n</body>"
+	construct += "\n</div>\n</div>\n<div id=\"mw-navigation\"><div id=\"mw-panel\"></div></div>\n</body>"
 	construct += footer
 
-	out = open(wikiName + "/" + name + ".html", "w")
+	out = open(name + ".html", "w")
 	out.write(construct)
 	out.close()
+# ~CreatePage
+
+def IsFolder(name: str):
+	return name.find(".") == -1
+
+def GatherWikiFiles(dir, currentDir: str):
+	global listOfPages
+	for f in dir:
+		if f.endswith(".wiki"):
+			print ("adding " + currentDir + "/" + f)
+			listOfPages.append(currentDir + "/" + f)
+		elif IsFolder(f):
+			folderPath = currentDir + "/" + f
+			GatherWikiFiles(os.listdir(folderPath), folderPath)
+
+def Main():
+	global wikiName, listOfPages
+	wikiName = input("Enter the name of your wiki: ")
+
+	dir = os.listdir(wikiName)
+
+	# Get all of the 
+	GatherWikiFiles(dir, wikiName)
+
+	for page in listOfPages:
+		CreatePage(page)
 	
-# Main
-
-wikiName = input("Enter the name of your wiki: ")
-
-#fullContent = "<!DOCTYPE html>\n<html>\n<head>\n<title>" + wikiName + "</title>\n</head>\n<body>\n\n<h1>This is a Heading</h1>\n<p>This is a paragraph.</p>\n\n</body>\n</html>"
-
-dir = os.listdir(wikiName)
-print(dir)
-
-for f in dir:
-	if f.endswith(".txt"):
-		CreatePage(f)
 
 # ~Main
+
+Main()
